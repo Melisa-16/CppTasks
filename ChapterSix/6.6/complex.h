de6d6d97ceac64e00432fcae37cf0c876c891d6b
Melisa-16 <melisabeglaryan8@gmail.com>
2026-03-13 17:15:30 +0400

    Update

--- ChapterSix/6.6/complex.h
+++ ChapterSix/6.6/complex.h
@@ -6,7 +6,7 @@ private:
 	float realPart;
 	float imaginaryPart;
 public:
-	complex(float,float);
+	complex(float = 1,float = 1);
 	void adding(complex,complex);
 	void substraction(complex,complex);
 	void print();
